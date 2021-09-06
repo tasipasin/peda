750539ae035100c8edd10cb49c2d8062c21b51da
Tasi Pasin <tasi.pasin96@gmail.com>
2021-09-06 15:10:24 -0300

    Inserção do arquivo de preparação para a atividade do Labirinto Usando fila

---
+++ labyrinth/main.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+#include<string>
+
+using namespace std;
+
+vector<string> labirinto = {
+	"XXXXXXXXXXXXXXXXXXXXX",
+	"X     X     X     X X",
+	"XX XX XXXXX X X X   X",
+	"X   X       XXX XXX X",
+	"X X X XXXXXXX   X   X",
+	"X X   X   X X X   X X",
+	"X XXXXX X X   XXXXX X",
+	"X X     X XXX  X    X",
+	"X X X X X   XX XX XXX",
+	"X X XXX X X     X   X",
+	"E X X X X XXXXX XXX X",
+	"X     X X   X X X   X",
+	"X XXXXX XXX X XXX X X",
+	"X X       X X   X X X",
+	"X   XXX X X XXX X X X",
+	"XXX X X X X X X X XXX",
+	"X X   X X   X   X   X",
+	"X XXX XXXXXXXXX XXX X",
+	"X                 X X",
+	"XXXXXXXXXXXXXXXXXXXSX"
+};
+
+int main() {}
